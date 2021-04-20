120f1995b2a6d68ab7571816fc3294e17e7c420f
Jawad Usman <jwdusmn@gmail.com>
2021-04-20 01:13:41 -0400

    stuff

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    [SerializeField] List<GameObject> enemies; // 0 = trangle; 1 = circle; 2 = square; 3 = triangle elite; 4 = cirle elite; 5 = square elite;
+
+    [SerializeField] float triangleChance, circleChance, squareChance, eTriangleChance, eCircleChance, eSquareChance;
+
+    [SerializeField] float spawnTimer, spawnTimerDefault;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnTimer <= 0)
+        {
+            SpawnEnemy();
+        }
+
+        spawnTimer -= Time.deltaTime;
+    }
+
+    void SpawnEnemy()
+    {
+        spawnPoint.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-3f, 3f));
+
+        int rand = Random.Range(0, 100);
+
+        if(rand < triangleChance)
+        {
+            Instantiate(enemies[0], spawnPoint.position, spawnPoint.rotation);
+        }
+        else if(rand < circleChance)
+        {
+            Instantiate(enemies[1], spawnPoint.position, spawnPoint.rotation);
+        }
+        else if(rand < squareChance)
+        {
+            Instantiate(enemies[2], spawnPoint.position, spawnPoint.rotation);
+        }
+        else if(rand < eTriangleChance)
+        {
+            Instantiate(enemies[3], spawnPoint.position, spawnPoint.rotation);
+        }
+        else if(rand < eCircleChance)
+        {
+            Instantiate(enemies[4], spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            Instantiate(enemies[5], spawnPoint.position, spawnPoint.rotation);
+
+        }
+
+        spawnTimer = spawnTimerDefault;
+    }
+}
