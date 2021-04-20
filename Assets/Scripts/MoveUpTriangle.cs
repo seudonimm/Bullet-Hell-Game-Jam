cf2249b71cd43aa43de0b05d7ee745b132d52b01
Malik Dobson <malikdobson1997@gmail.com>
2021-04-20 01:12:16 -0400

    Bouncy hell

---
+++ Assets/Scripts/MoveUpTriangle.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpTriangle : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] SpriteRenderer spr;
+    [SerializeField] LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spr.color = new Color(Random.Range(0.8f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Destroy Box"))
+        {
+            gameObject.SetActive(false);
+
+        }
+
+    }
+}
